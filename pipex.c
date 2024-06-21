6f507e7c5502776fa07054e3f44075ba5132055c
Eden Judah Amsalem <eamsalem@c1r4s4.42london.com>
2024-06-21 15:53:26 +0100

    version with buffer that I read output of first cmd to

--- pipex.c
+++ pipex.c
@@ -14,23 +14,114 @@
 
 // execve() 	- executes the specified program with the specified argv and envp variables
 //				  (if envp == NONE, the same environment parameters are used)
-// pipe()		- redirects an output as input to another process
+//				  this function replaces the current process image
+
 // dup()		- creates a copy of a file descriptor using the lowest, unused number
+
 // dup2()		- creates a copy of a file descriptor using the number passed as arg
-// perror()		- writes the error message arg to stderror
-// strerror()	- returns a pointer to the textual representation of the error number arg
-// access()		- checks whether a file is accessible for the specified mode
+//				  can use this to redirect printf from stdoutput to file2
+
 // fork()		- creates a new (child) process by duplicating the calling process
+//			  	  can be used to stop execve from overwriting our main process image
+
+// pipe()		- redirects an output as input to another process
+//				  can create a communication channel between parent & child processes
+
+// perror()		- writes the error message arg to stderror
+
+// strerror()	- returns a pointer to the textual representation of the error number arg
+
+// access()		- checks whether a file is accessible for the specified mode
+
 // unlink()		- removes the link to the specified file
 //				  (if the no. fd's for a file drops to 0, its content can be overwritten)
+
 // wait()		- suspend the calling process until a child process is complete
+
 // waitpid()	- same as wait() but specifies the specific child process ID (PID)
 
 
-//PSEUDO CODE:
-// Read the contents of file_1 and pass them as input to cmd1 X
-// Pipe the output of cmd1 to cmd2
-// Write the output of cmd2 to file_2
+// I can use the which command to find the path of any shell command
+
+
+void	ft_exec(char *cmd_path, char **exec_args, char *output)
+{
+	pid_t	pid;
+	int		fd[2];
+	char	*tmp;
+
+	if (pipe(fd) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+	if ((pid = fork()) < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1], 1);
+		if (!execve(cmd_path, exec_args, NULL))
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		close(fd[1]);
+	}
+	close(fd[1]);
+	wait(NULL);
+	while ((tmp = get_next_line(fd[0])))
+	{
+
+		ft_strlcat(output, tmp, ft_strlen(tmp) + ft_strlen(output) + 1);
+		free(tmp);
+	}
+	close(fd[0]);
+//	write(1, "1\n", 2);
+}
+
+char	**get_exec_args(char *input, char **cmd_args)
+{
+	char	**exec_args;
+	int		arg_count;
+	int		i;
+	int		j;
+
+	arg_count = ft_2D_arrlen(cmd_args);
+	exec_args = malloc(sizeof(char *) * (arg_count + 2));
+	i = 0;
+	j = 0;
+	while (i < arg_count)
+		exec_args[j++] = cmd_args[i++];
+	exec_args[j++] = input;
+	exec_args[j] = NULL;
+	return (exec_args);
+}
+
+int	pass_infile_to_cmd1(char *input, char *cmd1, char *output)
+{
+	char	*cmd1_path;
+	char	**cmd1_args;
+	char	**exec_args;
+
+	cmd1_args = ft_split(cmd1, ' ');
+	if (!cmd1_args)
+		return (0);
+	cmd1_path = ft_strjoin("/bin/", cmd1_args[0]);
+	if (!cmd1_path)
+		return (0);
+	exec_args = get_exec_args(input, cmd1_args);
+	if (!exec_args)
+		return (0);
+	ft_exec(cmd1_path, exec_args, output);
+	free_2D_arr(cmd1_args, true);
+	//free_2D_arr(exec_args, false);
+	free(cmd1_path);
+	return (1);
+}
 
 char	**get_cmds(int argc, char **argv)
 {
@@ -39,7 +130,7 @@ char	**get_cmds(int argc, char **argv)
 	int		j;
 	int		len;
 
-	cmds = malloc(sizeof(char *) * argc - 2);
+	cmds = malloc(sizeof(char **) * argc - 2);
 	if (!cmds)
 		return (NULL);
 	i = 2;
@@ -54,62 +145,51 @@ char	**get_cmds(int argc, char **argv)
 	return (cmds);
 }
 
-char	**get_exec_args(char *file1, char **cmd_args)
+bool	check_input(int argc, char **argv)
 {
-	char	**exec_args;
-	int		arg_count;
-	int		i;
-	int		j;
+	int		fd1;
+	int		fd2;
+	char	*buf;
 
-	arg_count = ft_arrlen(cmd_args);
-	exec_args = malloc(sizeof(char *) * (arg_count + 3));
-	exec_args[0] = "<";
-	exec_args[1] = file1;
-	i = 0;
-	j = 2;
-	while (i < arg_count)
-		exec_args[j++] = cmd_args[i++];
-	exec_args[j] = NULL;
-	return (exec_args);
-}
-
-int	pass_file1_to_cmd1(char *file1, char *cmd1)
-{
-	char	*cmd_path;
-	char	**cmd_args;
-	char	**exec_args;
+	// check if the first and last argv are actually files
+	fd1 = open(argv[0], O_RDONLY);
+	fd2 = open(argv[argc - 1], O_WRONLY);
+	buf = NULL;
+	if (read(fd1, buf, 0) == -1)
+		return (false);
+	if (read(fd2, buf, 0) == -1)
+		return (false);
 	
-	cmd_args = ft_split(cmd1, ' ');
-	cmd_path = ft_strjoin("/bin/", cmd_args[0]);
-	exec_args = get_exec_args(file1, cmd_args);
-	if (!execve(cmd_path, exec_args, NULL))
-	{
-		perror("execve");
-		return (-1);
-	}
-	free_2D_arr(cmd_args);
-	free_2D_arr(exec_args);
-	free(cmd_path);
-	return (1);
+	// check if correct number of args
+	if (argc < 5)
+		return (false); // write error ?
+	
+	// check if cmds are valid shell commands ?? Might have to catch this when I find the path
+	return (true);
 }
 
 int	main(int argc, char **argv)
 {
-	char	*file1;
-	char	*file2;
+	char	*infile;
+//	char	*outfile;
 	char	**cmds;
-//	int		fd1;
-//	int		fd2;
-	
-	if (argc < 5)
-		return (-1); // write error ?
-	file1 = argv[1];
-	file2 = argv[argc - 1];
+	char	*output = malloc(sizeof(char) * 1000);
+//	int		i;
+
+//	if (!check_input(argc, argv))
+//		return (-1);
+	infile = argv[1];
+//	outfile = argv[argc - 1];
 	cmds = get_cmds(argc, argv);
-	pass_file1_to_cmd1(file1, cmds[0]);
-		
-//	fd1 = open(file1, O_RDONLY);
-//	fd2 = open(file2, O_WRONLY);
-//	pipex(fd1, fd2);
-	free_2D_arr(cmds);
+	if (!cmds)
+		return (-1);
+	output[0] = '\0';
+	pass_infile_to_cmd1(infile, cmds[0], output);
+	ft_printf("%s\n", output);
+	free(output);
+//	i = 1;
+//	while (cmds[i])
+//		exec(cmds[i]);
+//	read_cmd_to_outfile(outfile, cmds[i]);
+//	free_mem();
 }
